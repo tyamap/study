024904aa8187ea491ab4a0a00bb1e59f1ed64224
yamapi <tom333my@outlook.com>
2019-12-08 15:04:31 +0900

    '構成コメントの修正'

--- day1.py
+++ day1.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+﻿get_ipython().run_line_magic('matplotlib', 'inline')
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,20 +7,11 @@ a = 1.5 # a；定数
 x = np.linspace(-1,1) # x：変数 -1から1の範囲
 y = a*x # y：変数
 
-
-# In[4]:
-
-
 plt.plot(x,y)
 plt.xlabel("x",size=14)
 plt.ylabel("y",size=14)
 plt.grid()
 plt.show()
-
-
-# In[6]:
-
-
 
 def my_func(x):
     return 3*x + 2
@@ -35,11 +20,8 @@ x = 4
 y = my_func(x)
 print(y)
 
-
-# In[16]:
-
-
-print("xのべき乗")
+# =========================
+# べき乗の実装
 def my_func(x):
     a=3
     return x**3
@@ -54,11 +36,8 @@ plt.title("power")
 plt.grid()
 plt.show()
 
-
-# In[14]:
-
-
-print("xの平方根")
+# =========================
+# 平方根の実装
 def my_func(x):
     return np.sqrt(x)
 
@@ -72,10 +51,8 @@ plt.title("root")
 plt.grid()
 plt.show()
 
-
-# In[17]:
-
-
+# =========================
+# 演習
 def my_func(x):
     return 3*x**2 + 2*x + 3
 x= np.linspace(-2,2)
@@ -87,10 +64,6 @@ plt.ylabel("y")
 plt.title("polynomial_2")
 plt.grid()
 plt.show()
-
-
-# In[18]:
-
 
 def my_func(x):
     return 4*x**3 + 2*x**2 + x + 3
@@ -104,10 +77,6 @@ plt.title("polynomial_3")
 plt.grid()
 plt.show()
 
-
-# In[25]:
-
-
 def my_func(x):
     return x**3 - 2*x**2 - 3*x + 4
 x= np.linspace(-4,4)
@@ -119,4 +88,3 @@ plt.ylabel("y")
 plt.title("polynomial_4")
 plt.grid()
 plt.show()
-
