ec9134a7545569ecb0f694fcb617aefef8915c13
tyamap <tom333my@outlook.com>
2019-12-30 17:08:53 +0900

    [test] about state

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 export default class Title extends React.Component{
+	constructor(){
+		super();
+		// Reactコンポーネントが持つ状態データ。コンポーネントをどのようにレンダリングするかといった情報を格納する。
+		// setState()を通じて字変更されることで再レンダリングの命令がキューイングされて自動的にコンポーネントの再レンダリングがなされる。
+		this.state = {name :"tyamap"};
+	}
+
 	render(){
+		setTimeout(
+			() => {this.setState({name: "Welcome!"});}
+			, 2000);
 		return(
-			<h1>Welcome!</h1>
+			<h1>{this.state.name}</h1>
 		);
 	}
 }
