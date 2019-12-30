105b02859c71741abe07d19172b9dfd97f3e105a
tyamap <tom333my@outlook.com>
2019-12-30 15:02:02 +0900

    [test] about JSX

--- src/js/client.js
+++ src/js/client.js
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
 	render() {
+		let name = "Guest";
+		let a = 2;
+		let b = 3;
 		return (
-			<h1>Hello React!</h1>
+			<div>
+				<h1>Welcome {name}!</h1>
+				<h2>It works!</h2>
+				<p>
+				a = {a}, b = {b}<br/>
+				a + b =<br/> 
+				inline: {a + b}<br/>
+				function: {this.get_sum(a,b)}<br/>
+				anonymous: {((a,b) => {return a + b;})(a,b)}
+				</p>
+			</div>
 		);
+	}
+	get_sum(a, b) {
+		return a + b;
 	}
 }
 
