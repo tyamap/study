8751722eb58235c2ba4bfeef9b0d24f06160ed58
tyamap <tom333my@outlook.com>
2020-01-01 12:42:56 +0900

    [test] stateとprop

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -8,15 +8,21 @@ export default class Layout extends React.Component {
 		this.name = "tyamap";
 		this.a = 2;
 		this.b = 3;
+		this.state = {title: "Welcome!"};
 	}
 
 	render() {
 		let a = this.a;
 		let b = this.b;
-		const title = "Welcome tyamap!";
+		setTimeout(
+			()=> {this.setState({title: "Welcome tyamap!"}); },
+			2000
+		);
+		
 		return (
 			<div>
-				<Header title={title} />
+				<Header title={this.state.title} />
+				<Header title={"Thank you!"} />
 				<h1>Welcome {this.name}!</h1>
 				<h2>It works!</h2>
 				<p>
