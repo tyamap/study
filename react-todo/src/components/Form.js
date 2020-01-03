57924e26f94ed93ab50cdcaa800f0f94336ce41c
tyamap <tom333my@outlook.com>
2020-01-03 11:37:44 +0900

    [test] simple to-do app

---
+++ react-todo/src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Form = (proprs) => (
+	<form className="siimlre-form">
+		<div className="siimple-form-field">
+			<label className="siimple-label siimple--color-white">Your todo:</label>
+			<input name="title" type="text" className="siimple-input"/>
+			<input type="submit" value="Add" className="siimple-btn siimple-btn--teal"/>
+		</div>
+	</form>
+);
+
+export default Form;
