958b838b1459238f349b824d115a3b0994309435
tyamap <tom333my@outlook.com>
2020-01-03 11:56:35 +0900

    [add] リスト表示

--- react-todo/src/components/App.js
+++ react-todo/src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import List from './List';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className="siimple-box siimple--bg-dark">
-        <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
-		    <Form/>
-      </div>
+	render() {
+		return (
+			<div className="siimple-box siimple--bg-dark">
+				<h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+				<Form/>
+				<div className="siimple-rule"></div>
+				<List/>
+			</div>
     );
   }
 }
