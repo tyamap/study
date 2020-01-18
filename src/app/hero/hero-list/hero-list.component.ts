7c85adebebaac81687cf8f6abe6154a9d09ba28b
tyamap <tom333my@outlook.com>
2020-01-18 11:35:29 +0900

    routing by variable

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-list.component.scss']
 })
 export class HeroListComponent implements OnInit {
+  id = '1';
 
   constructor() { }
 
