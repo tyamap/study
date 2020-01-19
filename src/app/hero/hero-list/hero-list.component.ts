ac497a2d842cb945c13f80ae9adbc15d0677007d
tyamap <tom333my@outlook.com>
2020-01-19 14:50:39 +0900

    Created operation-button

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/shared/models/hero';
 import { HeroService } from 'src/app/shared/services/hero.service';
 
+// ホバー情報を保持するための拡張クラス
+class HeroListElement extends Hero {
+  hovered: boolean;
+}
+
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
   styleUrls: ['./hero-list.component.scss']
 })
 export class HeroListComponent implements OnInit {
-  heros: Hero[] = null;
+  heros: HeroListElement[] = null;
 
   constructor(
     // コンストラクターの引数でServiceを呼ぶことで、コンポーネントやサービスに注入できる。
@@ -19,8 +24,20 @@ export class HeroListComponent implements OnInit {
     // subscribeで対象のObservableを観測する。観測が完了すると、引数内の処理がコールバックされる。
     this.heroService.list().subscribe(
       (heros: Hero[]) => {
-        this.heros = heros;
+        // 配列内の各要素に対して処理をする、mapメソッドを利用。
+        this.heros = heros.map(
+          (hero: Hero) => {
+            return{
+              // ...はSpreadOpertater。heroに対して、hovered: false を追加しているという書き方
+              ... hero,
+              hovered: false,
+            };
+          }
+        );
       }
     );
   }
+
+  hovered(hero: HeroListElement): void { hero.hovered = true; }
+  unhovered(hero: HeroListElement): void { hero.hovered = false; }
 }
