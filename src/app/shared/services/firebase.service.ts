206e3fd6ffbde2966e7d8ae116191ba60fcdd9a0
tyamap <tom333my@outlook.com>
2020-01-21 22:49:43 +0900

    Using firebase realtimarDatabase

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -1,36 +1,38 @@
-// import { Injectable } from '@angular/core';
-// import * as firebase from 'firebase';
-// import UserCredential = firebase.auth.UserCredential;
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import UserCredential = firebase.auth.UserCredential;
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class FirebaseService {
-//   EMAIL = 'email@example.com';
-//   PASSWORD = 'password';
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  // EMAIL = 'email@example.com';
+  // PASSWORD = 'password';
+  ltdb: firebase.database.Database;
 
-//   constructor() {
-//     const config = {
-//       /**
-//        *
-//        *
-//        *   firebaseのapiキー
-//        *
-//        *
-//        */
-//     };
-//     firebase.initializeApp(config);
-//     this.signInOrCreateUser(this.EMAIL, this.PASSWORD);
-//   }
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyDW0Wd9i2aVcCd-BUSp5V9k7FCMDP9kwKI',
+      authDomain: 'hello-heroes.firebaseapp.com',
+      databaseURL: 'https://hello-heroes.firebaseio.com',
+      projectId: 'hello-heroes',
+      storageBucket: 'hello-heroes.appspot.com',
+      messagingSenderId: '385562054185',
+      appId: '1:385562054185:web:b3ebf092b0e075456b9638'
+    };
+    firebase.initializeApp(config);
+    // this.signInOrCreateUser(this.EMAIL, this.PASSWORD);
+    this.ltdb = firebase.database();
+  }
 
-//   // firebase未登録なら登録処理。登録済みならログイン処理
-//   signInOrCreateUser(email: string, password: string): void {
-//     firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
-//       console.log(userCredential.user.uid);
-//     }).catch(() => {
-//       firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
-//         console.log(userCredential.user.uid);
-//       });
-//     });
-//   }
-// }
+  // firebase未登録なら登録処理。登録済みならログイン処理
+  // signInOrCreateUser(email: string, password: string): void {
+  //   firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
+  //     console.log(userCredential.user.uid);
+  //   }).catch(() => {
+  //     firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential: UserCredential) => {
+  //       console.log(userCredential.user.uid);
+  //     });
+  //   });
+  // }
+}
--- src/app/shared/services/hero.service.ts
+++ src/app/shared/services/hero.service.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@angular/core';
 import { Hero } from '../models/hero';
 import { Observable, of } from 'rxjs/index';
 import { HeroEditComponent } from 'src/app/hero/hero-edit/hero-edit.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  heroes = [
-    new Hero('H00001', 'スパイダーマン', 'ウェブシュート', 'あなたの親愛なる隣人、スパイディ！'),
-    new Hero('H00002', 'アイアンマン', 'リパルサーレイ', '私が アイアンマンだ。'),
-    new Hero('H00003', 'キャプテン・アメリカ', 'シールドスラッシュ', 'いや、できるさ。僕はキャプテンだ！'),
-  ];
+  heroes = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   // RxJSを利用した非同期処理。
   // ObservableはRxJSのクラス。Hero配列をObservableなものとして返す。
   list(): Observable<Hero[]> {
-    return of(this.heroes);
+    return this.http.get(`https://hello-heroes.firebaseio.com/heroes.json`).pipe(
+      map((response: any) =>
+        Object.keys(response).map((key: string) => {
+          const hr = response[key];
+          return new Hero(hr.id, hr.name, hr.skill, hr.description);
+        })
+      )
+    );
   }
 
   // idで指定されたHeroのObsavableを返す。
