fc29da0ab82f8a8493acad1ebd7e1194313705f5
tyamap <tom333my@outlook.com>
2020-05-27 20:05:49 +0900

    キーボード入力

---
+++ Assets/Scenes/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// キーボード入力を行なってコンソールにログを出力する
+// - 方向キーの入力
+// - ボタンの入力
+public class Sample : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Debug.Log(x);
+    }
+}
