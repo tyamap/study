a8250c05ac1b114b2ef6969a5d1f6eee920cb928
tyamap <tom333my@outlook.com>
2020-05-29 17:36:53 +0900

    [add] DOMを対象にしたテスト

--- TryMvcTest/Scripts/Qunit/app-test.js
+++ TryMvcTest/Scripts/Qunit/app-test.js
@@ -2,7 +2,6 @@ QUnit.module("add 関数のテスト", {
   setup: function () {
   }
 });
-
 QUnit.test('成功するテスト', function (assert) {
   assert.equal(add(1, 1), 2)
   assert.equal(add(2, 3), 5)
@@ -19,3 +18,12 @@ QUnit.test('常に成功', function (assert) {
   assert.equal(true, true)
 });
 
+QUnit.module("getBoxTextのテスト", {
+  setup: function () {
+  }
+});
+QUnit.test("DOM取得のテスト", function(assert) {
+  assert.equal(getBoxText(1), 'box1要素のテキスト', 'DOM取得のテスト');
+  assert.equal(getBoxText(9), '該当するDOM要素がありません。', 'DOM取得のテスト');
+  assert.equal(getBoxText(2), 'box2要素のテキスト', 'DOM取得のテスト');
+});
--- TryMvcTest/Scripts/src/app.js
+++ TryMvcTest/Scripts/src/app.js
@@ -1,3 +1,9 @@
 function add(a, b) {
   return a + b;
 }
+
+// #box{num}要素ないのテキストを返す関数
+var getBoxText = function(num){
+  if ($('#box'+num)[0] == undefined) { return '該当するDOM要素がありません。' };
+  return $('#box'+num).text();
+};
