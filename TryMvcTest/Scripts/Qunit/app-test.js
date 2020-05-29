1f2be820b3c90e898c1e160545aaa0030127eb56
tyamap <tom333my@outlook.com>
2020-05-29 16:07:47 +0900

    [mod] テストの分類、スキップ

--- TryMvcTest/Scripts/Qunit/app-test.js
+++ TryMvcTest/Scripts/Qunit/app-test.js
@@ -1,8 +1,21 @@
-QUnit.test('add 関数のテスト', function (assert) {
+QUnit.module("add 関数のテスト", {
+  setup: function () {
+  }
+});
+
+QUnit.test('成功するテスト', function (assert) {
   assert.equal(add(1, 1), 2)
-  
   assert.equal(add(2, 3), 5)
-  
-  // 失敗するテスト
+});
+QUnit.skip('失敗するテスト', function (assert) {
   assert.equal(add(8, 2), 9)
 });
+
+QUnit.module("関係のないテスト", {
+  setup: function () {
+  }
+});
+QUnit.test('常に成功', function (assert) {
+  assert.equal(true, true)
+});
+
