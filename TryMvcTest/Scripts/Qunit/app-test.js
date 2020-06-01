206050ebcfaa28bda4c722eb65ac4d30631af648
tyamap <tom333my@outlook.com>
2020-06-01 14:59:10 +0900

    QUnit DOM操作のテスト

--- TryMvcTest/Scripts/Qunit/app-test.js
+++ TryMvcTest/Scripts/Qunit/app-test.js
@@ -23,7 +23,17 @@ QUnit.module("getBoxTextのテスト", {
   }
 });
 QUnit.test("DOM取得のテスト", function(assert) {
-  assert.equal(getBoxText(1), 'box1要素のテキスト', 'DOM取得のテスト');
-  assert.equal(getBoxText(9), '該当するDOM要素がありません。', 'DOM取得のテスト');
-  assert.equal(getBoxText(2), 'box2要素のテキスト', 'DOM取得のテスト');
+  assert.equal(getBoxText(1), 'box1要素のテキスト', 'DOM取得のテスト - 1');
+  assert.equal(getBoxText(9), '該当するDOM要素がありません。', 'DOM取得のテスト - 9');
+  assert.equal(getBoxText(2), 'box2要素のテキスト', 'DOM取得のテスト - 2');
+});
+
+QUnit.test("クリックイベントのテスト", function (assert) {
+  $(window).trigger('load');       // 無名関数定義のきっかけとなるイベントを発生させる
+
+  assert.equal(getBoxText(1), 'box1要素のテキスト', 'クリック前');
+  
+  $('#box1').trigger('click');       // クリックのイベントハンドラ実行
+  
+  assert.equal(getBoxText(1), 'クリックされました。', 'クリック後');
 });
