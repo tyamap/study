79bc9a5655b746daba66f8c5841a92f82aa998fa
tyamap <tom333my@outlook.com>
2020-09-16 19:12:44 +0900

    動的コンポーネント

--- vuejs/vueCLI/practice/src/App.vue
+++ vuejs/vueCLI/practice/src/App.vue
@@ -4,23 +4,30 @@
       <h2>はじめまして</h2>
     </LikeHeader>
     <h2>{{number}}</h2>
-    <LikeNumber :number="number" @my-click="number = $event"></LikeNumber>
     <LikeNumber :number="number"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
-import LikeHeader from './components/LikeHeader.vue'
+import LikeHeader from './components/LikeHeader.vue';
+import About from './components/About.vue';
+import Home from './components/Home.vue';
 
 export default{
   data: function () {
     return {
-      number: 14
+      number: 14,
+      currentComponent: "Home"
     }
   },
   // コンポーネントのローカル登録
   components: {
-    LikeHeader
+    LikeHeader,
+    Home,
+    About,
   }
 }
 </script>
