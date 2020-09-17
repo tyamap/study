bafb3eff04a372fffa0054228e0447904d95b04e
tyamap <tom333my@outlook.com>
2020-09-17 20:53:11 +0900

    カスタムディレクティブのローカル登録

--- vuejs/vueCLI/practice/src/components/Home.vue
+++ vuejs/vueCLI/practice/src/components/Home.vue
@@ -3,3 +3,22 @@
     <p v-border:solid.shadow.round="{width: '10px', color: 'red'}">Home</p>
   </div>
 </template>
+
+<script>
+export default {
+  directives: {
+    border (el, binding) {
+      el.style.border = 'solid black 2px'; // DOM要素に対する操作
+      el.style.borderWidth = binding.value.width; // 属性値を受け取る
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg; // 引数を受け取る
+      if (binding.modifiers.round) {
+        el.style.borderRadius = '0.5rem';
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = '0.2px 5px rgba(0, 0, 0, 0.26)';
+      }
+    }
+  }
+}
+</script>
--- vuejs/vueCLI/practice/src/main.js
+++ vuejs/vueCLI/practice/src/main.js
@@ -6,21 +6,6 @@ Vue.config.productionTip = false
 // コンポーネントのグローバル登録
 Vue.component('LikeNumber', LikeNumber)
 
-// カスタムディレクティブ v-border
-Vue.directive("border", function (el, binding) {
-  // bindとupdateフックに適用される
-  el.style.border = 'solid black 2px'; // DOM要素に対する操作
-  el.style.borderWidth = binding.value.width; // 属性値を受け取る
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg; // 引数を受け取る
-  if (binding.modifiers.round) {
-    el.style.borderRadius = '0.5rem';
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = '0.2px 5px rgba(0, 0, 0, 0.26)';
-  }
-})
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
