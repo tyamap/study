4ccca0493bd6ad2dca2e29206fa033f006f73ca0
tyamap <tom333my@outlook.com>
2020-09-18 16:26:04 +0900

    vuex導入と利用

--- vuejs/vueCLI/router/src/main.js
+++ vuejs/vueCLI/router/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -13,6 +14,7 @@ router.beforeEach((to, from, next) => {
 })
 
 new Vue({
-  router: router,
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
--- vuejs/vueCLI/router/src/views/Home.vue
+++ vuejs/vueCLI/router/src/views/Home.vue
@@ -2,11 +2,17 @@
   <div>
     <h1>Home</h1>
     <button @click="toUsers">User一覧</button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  },
   methods:{
     toUsers(){
       this.$router.push({path: 'users'});
