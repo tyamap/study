dea4f2d9684d92d5b5ba953c662554c52707d707
tyamap <tom333my@outlook.com>
2020-09-18 17:41:07 +0900

    fix

--- vuejs/vueCLI/router/src/views/Home.vue
+++ vuejs/vueCLI/router/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Home</h1>
     <button @click="toUsers">User一覧</button>
-    <p>{{ count }}</p>
+    <p>{{ getCount }}</p>
     <p>{{ doubleCount }}</p>
     <p>{{ tripleCount }}</p>
     <input type="text" v-model="message">
