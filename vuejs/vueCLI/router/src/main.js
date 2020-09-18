d2f155df0eb04e949331980cce52204f6e940166
tyamap <tom333my@outlook.com>
2020-09-18 14:01:57 +0900

    グローバルなナビゲーションガード

--- vuejs/vueCLI/router/src/main.js
+++ vuejs/vueCLI/router/src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  // ユーザー認証チェックなど
+  // if (hoge === null) {
+  //   next("/");
+  // }
+  next();
+})
+
 new Vue({
   router: router,
   render: h => h(App),
