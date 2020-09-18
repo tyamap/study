eff895f8fb6bd5e99a45a1d5fbe62b67b6364f77
tyamap <tom333my@outlook.com>
2020-09-18 13:05:13 +0900

    v-bindでURLを動的に

--- vuejs/vueCLI/router/src/views/User.vue
+++ vuejs/vueCLI/router/src/views/User.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="/users/+ (Number(id) + 1)">Next User</router-link>
+    <div>
+      <router-link :to="'/users/' + id + '/posts'" class="link">posts</router-link>
+      <router-link :to="'/users/' + id + '/profile'" class="link">profile</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
