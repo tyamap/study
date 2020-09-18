4f077db687f8fa71eab9425b9e2374a5ee9465bf
tyamap <tom333my@outlook.com>
2020-09-18 12:13:58 +0900

    $router.push

--- vuejs/vueCLI/router/src/views/Home.vue
+++ vuejs/vueCLI/router/src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1>Home</h1>
+    <button @click="toUsers">User一覧</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods:{
+    toUsers(){
+      this.$router.push({path: 'users'});
+    }
+  }
+}
+</script>
