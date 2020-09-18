a3a51fdfd6c5bb86bec49d0d577e1bef1185f01d
tyamap <tom333my@outlook.com>
2020-09-18 11:47:12 +0900

    router-link

--- vuejs/vueCLI/router/src/App.vue
+++ vuejs/vueCLI/router/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <router-view></router-view>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link to="/" class="link">Home</router-link>
+      <router-link to="/users" class="link">Users</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
+
+<style>
+  .link {
+    margin-right: 10px;
+  }
+</style>
