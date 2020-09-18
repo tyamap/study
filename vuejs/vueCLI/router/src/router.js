509ea85cb7176d6cc3a6577665eb6f57aa7f2de1
tyamap <tom333my@outlook.com>
2020-09-18 12:14:33 +0900

    URLの動的なparams

--- vuejs/vueCLI/router/src/router.js
+++ vuejs/vueCLI/router/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue'
+import User from './views/User.vue'
 import Users from './views/Users.vue'
 
 Vue.use(Router)
@@ -9,5 +10,7 @@ export default new Router({
   mode: "history",
   routes: [
     { path: '/', component: Home },
-    { path: '/users', component: Users}]
+    { path: '/users', component: Users },
+    { path: '/user/:id', component: User},
+  ]
 })
