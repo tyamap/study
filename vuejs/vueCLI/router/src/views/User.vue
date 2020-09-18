955948e35b78e4ed7e7f5539b9876baf37ed7aeb
tyamap <tom333my@outlook.com>
2020-09-18 12:14:48 +0900

    動的なparams

---
+++ vuejs/vueCLI/router/src/views/User.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>User No. {{ $route.params.id }}</h1>
+  </div>
+</template>
--- vuejs/vueCLI/router/src/views/Users.vue
+++ vuejs/vueCLI/router/src/views/Users.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <h1>Users</h1>
+    <router-link to="/user/1" class="link">User-1</router-link>
+    <router-link to="/user/2" class="link">User-2</router-link>
   </div>
 </template>
