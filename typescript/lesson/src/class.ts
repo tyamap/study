43870be83cd00b33d7d09d93070899a78f78f49f
tyamap <tom333my@outlook.com>
2020-09-24 12:15:18 +0900

    static

--- typescript/lesson/src/class.ts
+++ typescript/lesson/src/class.ts
@@ -1,4 +1,9 @@
 class Person {
+  static species = 'Homo sapiens';
+  static isAdult(age: number) {
+    if (age >= 20) return true;
+    return false;
+  }
   constructor(public readonly name: string, protected age: number) {
   }
 
@@ -39,3 +44,7 @@ const green = new Teacher('Green', 28, 'English');
 console.log(green);
 green.incrementAge();
 console.log(green.greeting());
+
+console.log(Person.species);
+console.log(Person.isAdult(32));
+console.log(Person.isAdult(18));
