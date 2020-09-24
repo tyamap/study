a2c2b6feb939596195ceccb072bccbbea65dac23
tyamap <tom333my@outlook.com>
2020-09-24 11:52:53 +0900

    private

--- typescript/lesson/src/class.ts
+++ typescript/lesson/src/class.ts
@@ -1,13 +1,19 @@
 class Person {
   name: string;
-  constructor(initName: string) {
+  private age: number;
+
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
 
   greeting(this: Person) {
-    console.log('Hello! My name is ${this.name}.');
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+  }
+  incrementAge() {
+    this.age += 1;
   }
 }
-const tommy = new Person('Tommy');
+const tommy = new Person('Tommy', 24);
 console.log(tommy);
 console.log(tommy.greeting());
