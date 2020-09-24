964c050df29afd9489f103e631a8a78ab02d7180
tyamap <tom333my@outlook.com>
2020-09-24 19:50:14 +0900

    keyof を用いて柔軟な関数を

--- typescript/lesson/src/generics.ts
+++ typescript/lesson/src/generics.ts
@@ -1,4 +1,5 @@
-function copy<T extends { name: string }>(value: T): T {
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+  console.log(value[key]);
   return value;
 }
-console.log(copy({name: 'hello'}));
+console.log(copy({ name: 'Tom', age: 22 }, 'age'));
