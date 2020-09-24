1d98ac736dbd398a2c02d62f1492d168b69746ed
tyamap <tom333my@outlook.com>
2020-09-24 12:03:57 +0900

    クラスの継承

--- typescript/lesson/src/class.ts
+++ typescript/lesson/src/class.ts
@@ -1,5 +1,5 @@
 class Person {
-  constructor(public readonly name: string, private age: number) {
+  constructor(public readonly name: string, protected age: number) {
   }
 
   greeting(this: Person) {
@@ -12,3 +12,17 @@ class Person {
 const tommy = new Person('Tommy', 24);
 console.log(tommy);
 console.log(tommy.greeting());
+
+class Teacher extends Person {
+  constructor(name: string, age: number, public subject: string) {
+    super(name, age);
+  }
+
+  greeting() {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`);
+  }
+}
+const green = new Teacher('Green', 28, 'English');
+console.log(green);
+green.incrementAge();
+console.log(green.greeting());
