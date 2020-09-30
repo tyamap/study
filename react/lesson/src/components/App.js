a61ff67dd3455656cfa5f433fabdd6805f99f557
tyamap <tom333my@outlook.com>
2020-09-30 15:43:43 +0900

    setup app

--- react/lesson/src/components/App.js
+++ react/lesson/src/components/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Hello, World!</h1>
-  );
+const App = () =>  (
+  <Counter></Counter>
+)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  hundlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  hundleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+
 }
 export default App;
