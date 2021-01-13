7132a58e78f8f6a1936ab0626cd1acedc817eb76
tyamap <tom333my@outlook.com>
2021-01-13 20:18:11 +0900

    だめじゃ

--- react/dynamicform/src/App.tsx
+++ react/dynamicform/src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TagForm from "./components/TagForm";
 
 function App() {
+  const [inputKey, setInputKey] = useState(0);
+  const [input, setInput] = useState(["tag-0"]);
+  const [tags, setTags] = useState([{ name: "", key: "tag-0" }]);
+
+  const addForm = () => {
+    const key = inputKey + 1;
+    setInputKey(key);
+  };
+
+  const deleteInput = (index: number) => {
+    const inputs = [...input]
+    inputs.splice(index, 1)
+    setInput(inputs)
+  }
+
+  const submit = () => {
+    
+  }
+
+  useEffect(() => {
+    console.log(tags);
+  }, [tags]);
+
+  useEffect(() => {
+    if (inputKey !== 0) {
+      console.log(inputKey);
+      setInput([...input, `tag-${inputKey}`]);
+    }
+  }, [inputKey]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>Form</h1>
+        {input.map((input, i) => (
+          <div key={input}>
+            <TagForm formKey={input} setTags={setTags} tags={tags} /><button onClick={() => {deleteInput(i)}}>-</button>
+          </div>
+        ))}
+      </form>
+      <button onClick={addForm}>+</button>
+      <button onClick={submit}>SUBMIT</button>
     </div>
   );
 }
